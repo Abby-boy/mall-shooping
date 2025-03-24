626356479058be93f789b9b5dda93c96ee68d67a
dwj <2278797260@qq.com>
2025-03-24 14:40:02 +0800

    feat:商城网关模块开发

---
+++ mall-gateway/src/test/java/com/dingjiajia/mall/gateway/MallGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dingjiajia.mall.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MallGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
